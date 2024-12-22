ccbc158aea24e4c8a8e4454c22d0a26ad0717f04
callioptsu <pasmac.dev@protonmail.com>
2024-12-22 12:58:07 -0300

    feat: rederization in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+// Components
+import { ComponentHeader } from '@/components'
+
 // Material Ui
 import { Box, Typography } from '@mui/material'
 
 export default function Home() {
   return (
     <Box component="main">
+      <ComponentHeader />
       <Typography component="h1">Name</Typography>
     </Box>
   )
