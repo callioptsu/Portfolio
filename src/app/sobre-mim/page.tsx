c4cf030a8678f11d3ea4dd8588930782c15adc7c
callioptsu <pasmac.dev@protonmail.com>
2024-12-30 19:21:53 -0300

    feat(about): starts about me page

--- src/app/sobre-mim/page.tsx
+++ src/app/sobre-mim/page.tsx
@@ -1,5 +1,10 @@
+import Image from 'next/image'
+
 // Material UI
-import { Box, Typography } from '@mui/material'
+import { Box, Link, Typography } from '@mui/material'
+
+// React Icons
+import { IoMdDownload } from 'react-icons/io'
 
 // Custom
 import { HeaderComponent } from '@/components'
@@ -8,8 +13,56 @@ export default function AboutMe() {
   return (
     <>
       <HeaderComponent />
-      <Box component="main">
-        <Typography component="h1">Sobre</Typography>
+      <Box
+        component="main"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Box component="section">
+          <Image
+            src="/img/photo.jpg"
+            alt="antonio santana"
+            width={150}
+            height={150}
+            style={{
+              borderRadius: '100%',
+              border: '2px solid rgb(0, 0, 0, 0.5)',
+            }}
+            priority
+          />
+        </Box>
+        <Box component="section">
+          <Link
+            href="/antonio-santana-cv.pdf"
+            target="_blank"
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              textDecoration: 'none',
+              cursor: 'pointer',
+              border: '2px solid red',
+            }}
+            download
+          >
+            <IoMdDownload />
+            BAIXAR CV
+          </Link>
+        </Box>
+        <Box component="section">
+          <Typography fontFamily="Poppins" variant="subtitle2">
+            Desde os 16 anos apaixonado por tecnologia. Ao longo dos anos me
+            dediquei a expandir meus conhecimentos em busca de novas
+            oportunidades que me permitissem crescer profissionalmente. Tenho
+            experiência em desenvolvimento front-end com React, bem como em
+            back-end com Node.js e Java. Meu foco é o aprendizado contínuo que
+            me fará contribuir em inúmeros projetos inovadores.
+          </Typography>
+        </Box>
+        <Box component="section">Timeline</Box>
       </Box>
     </>
   )
