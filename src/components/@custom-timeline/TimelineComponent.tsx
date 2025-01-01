ea39fe0e099b03e57a5dac6f6ac28349ecd8aa4f
callioptsu <pasmac.dev@protonmail.com>
2025-01-01 16:06:59 -0300

    feat: create custom timeline component

---
+++ src/components/@custom-timeline/TimelineComponent.tsx
@@ -0,0 +1,56 @@
+import Link from 'next/link'
+
+// Material Ui
+import { Typography } from '@mui/material'
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineDot,
+  TimelineConnector,
+  TimelineContent,
+} from '@mui/lab'
+
+interface TimelineComponentProps {
+  position: 'left' | 'right'
+  isFinished: boolean
+  courseUrl: string
+  institution: string
+  name: string
+  finishedDate: string
+  certificateUrl: string
+}
+
+export default function TimelineComponent({
+  position,
+  isFinished,
+  courseUrl,
+  institution,
+  name,
+  finishedDate,
+  certificateUrl,
+}: TimelineComponentProps) {
+  return (
+    <Timeline position={position}>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot color={isFinished ? 'success' : 'primary'} />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Link href={courseUrl} target="_blank" rel="noreferrer">
+            <Typography variant="caption" sx={{ cursor: 'pointer' }}>
+              {institution}
+            </Typography>
+          </Link>
+          <Typography variant="caption">{finishedDate}</Typography>
+          <Link href={certificateUrl} target="_blank" rel="noreferrer">
+            <Typography variant="caption" sx={{ cursor: 'pointer' }}>
+              {name}
+            </Typography>
+          </Link>
+        </TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as HeaderComponent } from './@custom-header/HeaderComponent'
 export { default as LinkComponent } from './@custom-links/LinkComponent'
 export { default as LeftbarComponent } from './@custom-leftbar/LeftbarComponent'
+export { default as TimelineComponent } from './@custom-timeline/TimelineComponent'
