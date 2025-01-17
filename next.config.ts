b28c31287484a8290c9093306935b648ed71a8eb
callioptsu <pasmac.dev@protonmail.com>
2025-01-17 00:29:45 -0300

    chore: adiciona placeholder e moment

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
     GITHUB_USER: process.env.GITHUB_USER,
     GITHUB_TOKEN: process.env.GITHUB_TOKEN,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
 }
 
 export default nextConfig
